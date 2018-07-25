689498f6cf29f1d7681a0b95cd349df2e597c92c
mohammedarqam <mohammedarqam97@gmail.com>
2018-07-26 01:36:44 +0530

    firest commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC-MuPFFSmsEX8WiR5PiNLea1UUuFEtyY4",
+  authDomain: "hacathon-969fe.firebaseapp.com",
+  databaseURL: "https://hacathon-969fe.firebaseio.com",
+  projectId: "hacathon-969fe",
+  storageBucket: "hacathon-969fe.appspot.com",
+  messagingSenderId: "870783797722"
+});
+
 
 @NgModule({
   declarations: [
