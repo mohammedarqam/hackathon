64e31e786dae274ec4944cd33fa4c55152dfa5dd
mohammedarqam <mohammedarqam97@gmail.com>
2018-07-26 07:24:06 +0530

    Slides Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as firebase from 'firebase';
+import { Slides } from 'ionic-angular';
 
 
 @Component({
@@ -8,11 +9,55 @@ import * as firebase from 'firebase';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+
+  quesRef = firebase.database().ref("Questions/");
+  questions : Array<any> = [];
+  curIndex : number = 1;
+  allSlides : number ;
 
   constructor(
   public navCtrl: NavController,
   ) {
+    this.getQuestions();
+  }
 
+  ionViewDidLoad(){
+    this.slides.lockSwipes(true);
+    document.getElementById("pBarId").style.width = '0';
+  }
+
+  getQuestions(){
+    this.quesRef.once('value',itemSnapshot=>{
+      this.questions = [];
+      itemSnapshot.forEach(itemSnap=>{
+        this.questions.push(itemSnap.val());
+        this.allSlides = this.questions.length;
+      })
+    })
+  }
+
+  slideChanged() {
+    this.curIndex = this.slides.getActiveIndex()+1;
+    var wid = (+this.curIndex/+this.allSlides)*100;
+    console.log(wid.toString());
+    document.getElementById("pBarId").style.width = wid.toString()+"%" ;
+  }
+
+
+
+
+
+  slidePrev() {
+    this.slides.lockSwipes(false);
+    this.slides.slidePrev(500);
+    this.slides.lockSwipes(true);
+  }
+
+  slideNext() {
+    this.slides.lockSwipes(false);
+    this.slides.slideNext(500);
+    this.slides.lockSwipes(true);
   }
 
 }
